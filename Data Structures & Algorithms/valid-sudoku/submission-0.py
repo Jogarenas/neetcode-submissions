44ee5d79fdeb9366a4299c2f7bc9467d360a2bb7
Jogarenas <jogarenas27@gmail.com>
2026-08-18 23:37:18 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                digit = board[r][c]
+
+                if digit == ".":
+                    continue
+
+                box_index = ((r // 3) * 3 + (c // 3))
+
+                if((digit in rows[r]) or (digit in cols[c]) or (digit in boxes[box_index])):
+                    return False
+
+                rows[r].add(digit)
+                cols[c].add(digit)
+                boxes[box_index].add(digit)
+        return True
